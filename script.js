88273c92d3da68b0d6345d23e07ce3ff7d4dacd4
abdservus <abdccassiano@hotmail.com>
2021-12-13 16:58:55 -0600

    Curso Javascript: Agregar proyecto a git

---
+++ script.js
@@ -0,0 +1,6 @@
+var nombre = prompt("Ingrese su nombre");
+var edad = prompt("Ingrese su edad");
+var telefono = "7352505677";
+var mensajeFinal = "Nombre: "+ nombre +". Edad: "+ edad +". Tel: "+ telefono;
+
+alert(mensajeFinal);
