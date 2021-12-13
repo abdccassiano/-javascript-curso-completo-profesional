5161b4d5914c2bdf00aef24df03cc6e61acffd60
abdservus <abdccassiano@hotmail.com>
2021-12-13 17:19:13 -0600

    Curso JavaScript: se agrego el documento .gitignore y se trabajo con LocalStorage básico ABD

--- scriptLocalStore.js
+++ scriptLocalStore.js
@@ -1,5 +1,29 @@
 window.onload = iniciar;
 
 function iniciar() {
-    
+    //Botón Agregar escucha el clic
+    var btnAgregar = document.getElementById("btnAgregar");
+    btnAgregar.addEventListener("click", clickBtnAgregar);
+
+    // Renderiza las notas
+    mostrarNotas();
+}
+
+function clickBtnAgregar() {
+    // Traemos al txtNotas
+    var txtNota = document.getElementById("txtNota");
+
+    //Queda asignado al localStorage
+    localStorage.nota = txtNota.value;
+
+    mostrarNotas();
+}
+
+function mostrarNotas() {
+
+    // Trae el div notas
+    var divNotas = document.getElementById("divNotas");
+    // Le asigna las notas desde el localStorage
+    divNotas.innerHTML = localStorage.nota;
+
 }
