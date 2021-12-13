9348d378b10be63f577bb1b5e97550f2960e9598
abdservus <abdccassiano@hotmail.com>
2021-12-13 17:39:55 -0600

    Curso JavaScript: Agregando un arreglo en LocalStorage

--- scriptLocalStore.js
+++ scriptLocalStore.js
@@ -3,7 +3,9 @@ window.onload = iniciar;
 function iniciar() {
     //Botón Agregar escucha el clic
     var btnAgregar = document.getElementById("btnAgregar");
+    var btnBorrar = document.getElementById("btnBorrar");
     btnAgregar.addEventListener("click", clickBtnAgregar);
+    btnBorrar.addEventListener("click", clickBtnBorrar);
 
     // Renderiza las notas
     mostrarNotas();
@@ -13,9 +15,16 @@ function clickBtnAgregar() {
     // Traemos al txtNotas
     var txtNota = document.getElementById("txtNota");
 
+    var notas = [];
+    if (localStorage.notas) {
+        notas = JSON.parse(localStorage.notas);
+    }
+    
+    notas.push(txtNota.value);
+    localStorage.notas = JSON.stringify(notas);
+    
     //Queda asignado al localStorage
-    localStorage.nota = txtNota.value;
-
+    //localStorage.nota = txtNota.value;
     mostrarNotas();
 }
 
@@ -24,6 +33,19 @@ function mostrarNotas() {
     // Trae el div notas
     var divNotas = document.getElementById("divNotas");
     // Le asigna las notas desde el localStorage
-    divNotas.innerHTML = localStorage.nota;
+    //divNotas.innerHTML = localStorage.nota;
+    var notas = [];
+    if (localStorage.notas) {
+        notas = JSON.parse(localStorage.notas);
+    }
+    var html = "";
+    for (var nota of notas) {
+        html += nota + "<br />";
+    }
+    divNotas.innerHTML = html;
+}
 
+function clickBtnBorrar() {
+    localStorage.clear();
+    mostrarNotas();
 }
